e8a5ce2b2b19b64441b540c1f947bcbb16f4377c
nakamurau1 <fyuichi@gmail.com>
2024-01-30 16:42:04 +0900

    協調的マルチタスクの実装

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,21 @@ extern crate alloc;
 use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::{memory::BootInfoFrameAllocator, println};
+use rust_os::{
+    memory::BootInfoFrameAllocator,
+    println,
+    task::Task,
+    task::{executor::Executor, keyboard, simple_executor::SimpleExecutor},
+};
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
 entry_point!(kernel_main);
 
@@ -26,29 +40,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // ヒープに数字をアロケートする
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // 動的サイズのベクタを作成する
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // 参照カウントされたベクタを作成する -> カウントが0になると解放される
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypress()));
+    executor.run();
 
     #[cfg(test)]
     test_main();
